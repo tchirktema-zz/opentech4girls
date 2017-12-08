e4141faf6c0b5c8150eb870f08ef9b9cef2cd0f6
Josué Tchirktema <tchirktemajosue@gmail.com>
2017-12-08 11:59:13 +0000

    Delete routes.rb

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  mount RailsAdmin::Engine => '/gesier', as: 'rails_admin'
-  root "registers#index"
-
-  resources :registers, only:[:index,:new,:create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
